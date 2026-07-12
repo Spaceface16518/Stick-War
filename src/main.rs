162936b84904a90b3f3bd670e5fbd811f748e98f
Amrit Rathie <amrit.rathie@gmail.com>
2026-07-11 21:34:18 -0600

    Enable live game config loading from bundled assets

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,30 @@ mod rendering;
 mod ui;
 mod units;
 
+use bevy::asset::AssetPlugin;
 use bevy::prelude::*;
 use game::GamePlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Stick War".into(),
-                resolution: (1280, 720).into(),
-                resizable: true,
-                canvas: Some("#bevy".into()),
-                fit_canvas_to_parent: true,
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(AssetPlugin {
+                    file_path: "config".into(),
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Stick War".into(),
+                        resolution: (1280, 720).into(),
+                        resizable: true,
+                        canvas: Some("#bevy".into()),
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
         .add_plugins(GamePlugin)
         .run();
 }
